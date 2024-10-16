5666bd83c5e804d7eeaace321d238ae9d6387a5d
sakthiveljayabal0088 <sakthiveljayabal23@gmail.com>
2024-10-16 18:05:12 +0530

    forgot pae api intergration

--- src/components/common/ForgotPage.js
+++ src/components/common/ForgotPage.js
@@ -1,85 +1,127 @@
 import React from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { Link } from "react-router-dom";
-
-const validationSchema = Yup.object({
-  email: Yup.string()
-    .email("*Invalid email address")
-    .required("*Email is required"),
-});
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import api from "../../config/URL";
 
 const ForgotPage = () => {
+  const navigate = useNavigate();
+
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email("*Invalid email address")
+      .required("*Email is required"),
+  });
+
   const formik = useFormik({
     initialValues: {
       email: "",
     },
     validationSchema: validationSchema,
-    onSubmit: (values) => {
-      console.log("values", values);
-      // Handle form submission, e.g., call an API
+    onSubmit: async (values) => {
+      // console.log("values", values);
+      try {
+        const response = await api.post("forgot-password", values);
+        if (response.status === 200) {
+          toast.success(response.data.message);
+          navigate("/forgotsuccess");
+        } else {
+          toast.error(response.data.message);
+        }
+      } catch (error) {
+        if (error.response && error.response.status === 422) {
+          const errors = error.response.data.error;
+          toast.error(errors.email);
+        } else {
+          toast.error("An error occurred. Please try again.");
+        }
+      }
     },
   });
 
   return (
-    <div className="container-fluid d-flex justify-content-center align-items-center mt-5">
-      <div className="card p-4 w-25 shadow-lg rounded-0">
-        <h3 className="text-center mb-3">Forgot Password</h3>
-        <p
-          className="text-center text-muted mb-4"
-          style={{ fontSize: "0.9rem" }}
-        >
-          Enter the email address or mobile phone number associated with your
-          account.
-        </p>
-
-        <form onSubmit={formik.handleSubmit}>
-          <div className="form-group mb-4 mt-2">
-            <label className="form-label" htmlFor="email">
-              Email Address
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className={`form-control rounded-0 ${
-                formik.touched.email && formik.errors.email
-                  ? "is-invalid"
-                  : formik.touched.email && !formik.errors.email
-                  ? "is-valid"
-                  : ""
-              }`}
-              placeholder="Email Address"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.email && formik.errors.email ? (
-              <div className="invalid-feedback mt-0">{formik.errors.email}</div>
-            ) : null}
-          </div>
-
-          <button
-            type="submit"
-            className="btn btn-primary btn-block mt-3 rounded-0 w-100"
-            style={{ backgroundColor: "#fa8232", borderColor: "#fa8232" }}
+    <section>
+      <div
+        className="container-fluid d-flex justify-content-center align-items-center vh-100"
+        style={{ backgroundColor: "rgb(242, 242, 242)" }}
+      >
+        <div className="row">
+          <div
+            className="card shadow-lg p-3 mb-5 rounded"
+            style={{ width: "100%", maxWidth: "400px" }}
           >
-            RESET PASSWORD
-          </button>
-        </form>
-
-        <div className="text-center mt-3 mb-4">
-          <Link to="/">
-            <p style={{ fontSize: "0.9rem" }}>
-              Go Back to{" "}
-              <span className="" style={{ color: "#fa8232" }}>
-                Sign In
-              </span>
+            <h3
+              className="cursor-pointer py-2 mb-3"
+              style={{
+                borderBottom: "2px solid #ef4444",
+                paddingBottom: "5px",
+                width: "100%",
+                textAlign: "center",
+                color: "#ef4444",
+              }}
+            >
+              Forgot Password
+            </h3>
+            <p
+              className="text-center text-muted mb-4"
+              style={{ fontSize: "0.9rem" }}
+            >
+              Enter the email address or mobile phone number associated with
+              your account.
             </p>
-          </Link>
+
+            <form onSubmit={formik.handleSubmit}>
+              <div className="form-group mb-4 mt-2">
+                <label className="form-label" htmlFor="email">
+                  Email Address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className={`form-control rounded-0 ${
+                    formik.touched.email && formik.errors.email
+                      ? "is-invalid"
+                      : formik.touched.email && !formik.errors.email
+                      ? "is-valid"
+                      : ""
+                  }`}
+                  placeholder="Email Address"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                />
+                {formik.touched.email && formik.errors.email ? (
+                  <div className="invalid-feedback mt-0">
+                    {formik.errors.email}
+                  </div>
+                ) : null}
+              </div>
+
+              {/* <Link to="/"> */}
+              <button
+                type="submit"
+                className="btn btn-primary btn-block mt-3 rounded-0 w-100"
+                style={{ backgroundColor: "#ef4444", borderColor: "#ef4444" }}
+              >
+                RESET PASSWORD
+              </button>
+              {/* </Link> */}
+            </form>
+
+            <div className="text-center mt-3 mb-4">
+              {/* <Link to="/"> */}
+              <p className="text-muted" style={{ fontSize: "0.9rem" }}>
+                Go Back to &nbsp;
+                <span style={{ color: "#ef4444" }}>Login In</span>
+              </p>
+              {/* </Link> */}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
