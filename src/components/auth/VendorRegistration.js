9e595e01e650d6c7feb92d06eba44eb5012f11e9
Chandru0811 <chandru08112000@gmail.com>
2024-11-23 17:17:58 +0530

    committed

--- src/components/auth/VendorRegistration.js
+++ src/components/auth/VendorRegistration.js
@@ -53,51 +53,51 @@ export default function VendorRegistration({ handleVendorLogin }) {
       className="container-fluid  m-0 vh-100"
       style={{ minHeight: "100vh", backgroundColor: "#f2f2f2" }}
     >
-       <div
+      <div
         className="d-flex justify-content-center align-items-center m-0 pt-2 mb-4"
         style={{ backgroundColor: "rgb(242, 242, 242)" }}
       >
         <img src={headerlogo} className="img-fluid" alt="img" />
       </div>
       <div className=" d-flex  justify-content-center align-items-center mt-5">
-      <div className="container">
-        <h2
-          className="d-flex justify-content-center"
-          style={{ color: "#ff0060" }}
-        >
-          Dealsmachi - Deals that's Matter !
-        </h2>
-        <Stepper className="mt-5" activeStep={activeStep} alternativeLabel>
-          {steps.map((step, index) => (
-            <Step key={index}>
-              <StepLabel>{step}</StepLabel>
-            </Step>
-          ))}
-        </Stepper>
-        <div
-          className="container-fluid card shadow-lg border-0 mb-4 d-flex justify-content-center align-items-center"
-          style={{ minHeight: "70vh", width: "70%" }}
-        >
-          <React.Fragment>
-            {activeStep === 0 && (
-              <Form1
-                formData={formData}
-                ref={childRef}
-                setFormData={setFormData}
-                handleNext={handleNext}
-                setLoadIndicators={setLoadIndicator}
-              />
-            )}
-            {activeStep === 1 && (
-              <Form4
-                formData={formData}
-                ref={childRef}
-                setFormData={setFormData}
-                handleNext={handleNext}
-                setLoadIndicators={setLoadIndicator}
-              />
-            )}
-            {/* {activeStep === 2 && (
+        <div className="container">
+          <h2
+            className="d-flex justify-content-center"
+            style={{ color: "#ff0060" }}
+          >
+            Dealsmachi - Deals that's Matter !
+          </h2>
+          <Stepper className="mt-5" activeStep={activeStep} alternativeLabel>
+            {steps.map((step, index) => (
+              <Step key={index}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <div
+            className="container-fluid card shadow-lg border-0 mb-4 d-flex justify-content-center align-items-center"
+            style={{ minHeight: "70vh", width: "70%" }}
+          >
+            <React.Fragment>
+              {activeStep === 0 && (
+                <Form1
+                  formData={formData}
+                  ref={childRef}
+                  setFormData={setFormData}
+                  handleNext={handleNext}
+                  setLoadIndicators={setLoadIndicator}
+                />
+              )}
+              {activeStep === 1 && (
+                <Form4
+                  formData={formData}
+                  ref={childRef}
+                  setFormData={setFormData}
+                  handleNext={handleNext}
+                  setLoadIndicators={setLoadIndicator}
+                />
+              )}
+              {/* {activeStep === 2 && (
               <Form2
                 formData={formData}
                 ref={childRef}
@@ -106,48 +106,50 @@ export default function VendorRegistration({ handleVendorLogin }) {
                 setLoadIndicators={setLoadIndicator}
               />
             )} */}
-            {activeStep === 2 && (
-              <Form3
-                formData={formData}
-                ref={childRef}
-                setFormData={setFormData}
-                handleNext={handleNext}
-                setLoadIndicators={setLoadIndicator}
-                handleVendorLogin={handleVendorLogin}
-              />
-            )}
-            <div className="container-fluid p-1 d-flex align-items-center justify-content-center gap-2">
-              {activeStep > 0 && (
-                <button
-                  className="btn btn-secondary mb-3" style={{width:"100%"}}
-                  onClick={handleBack}
-                >
-                  Back
-                </button>
+              {activeStep === 2 && (
+                <Form3
+                  formData={formData}
+                  ref={childRef}
+                  setFormData={setFormData}
+                  handleNext={handleNext}
+                  setLoadIndicators={setLoadIndicator}
+                  handleVendorLogin={handleVendorLogin}
+                />
               )}
-
-              {activeStep !== steps.length - 1 && (
-                <>
+              <div className="container-fluid p-1 d-flex align-items-center justify-content-center gap-2">
+                {activeStep > 0 && activeStep < steps.length - 1 && (
                   <button
-                    type="submit" style={{width:"100%"}}
-                    onClick={handleButtonClick}
-                    className="btn btn-button mb-3 w-100"
-                    disabled={loadIndicator}
+                    className="btn btn-secondary mb-3"
+                    style={{ width: "100%" }}
+                    onClick={handleBack}
                   >
-                    {loadIndicator && (
-                      <span
-                        className="spinner-border spinner-border-sm me-2"
-                        aria-hidden="true"
-                      ></span>
-                    )}
-                    {activeStep === steps.length - 2 ? "Submit" : "Continue"}
+                    Back
                   </button>
-                </>
-              )}
-            </div>
-          </React.Fragment>
+                )}
+
+                {activeStep !== steps.length - 1 && (
+                  <>
+                    <button
+                      type="submit"
+                      style={{ width: "100%" }}
+                      onClick={handleButtonClick}
+                      className="btn btn-button mb-3 w-100"
+                      disabled={loadIndicator}
+                    >
+                      {loadIndicator && (
+                        <span
+                          className="spinner-border spinner-border-sm me-2"
+                          aria-hidden="true"
+                        ></span>
+                      )}
+                      {activeStep === steps.length - 2 ? "Submit" : "Continue"}
+                    </button>
+                  </>
+                )}
+              </div>
+            </React.Fragment>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
