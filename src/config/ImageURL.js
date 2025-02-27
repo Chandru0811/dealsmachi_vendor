952da585ce88e6da8db3ea20e004888697aec98d
Chandru0811 <chandru08112000@gmail.com>
2025-02-27 19:18:25 +0530

    Image URL Changed

--- src/config/ImageURL.js
+++ src/config/ImageURL.js
@@ -1,4 +1,4 @@
-const ImageURL = "https://dealsmachi.com/";
-// const ImageURL = "http://127.0.0.1:8000/api/";
+// const ImageURL = "https://dealsmachi.com/";
+const ImageURL = "http://127.0.0.1:8000/api/";
 
 export default ImageURL;
